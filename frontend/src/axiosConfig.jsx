5a08ce6434d3c8cf7c1fc2f98722cd71b49c20fe
Md Mahfujur Rahman <mrrajuiit@gmail.com>
2025-03-04 05:26:22 +1000

    set up local baseURL

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.26.96.188:5001', // live
+  baseURL: 'http://localhost:5001', // local
+  //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
